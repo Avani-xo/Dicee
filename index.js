58cfbdff777583ca209240442ebab9dbb7a058be
Avani-xo <130911770+Avani-xo@users.noreply.github.com>
2024-11-17 23:51:32 +0530

    Update index.js (paths)

--- index.js
+++ index.js
@@ -5,27 +5,27 @@ ran = Math.floor(ran*6)+1;
 
 
 if(ran===1){
-    image1 = "./images/dice" + ran + ".png";
+    image1 = "./dice" + ran + ".png";
     document.querySelectorAll("img")[0].setAttribute("src", image1);
 }
 if(ran===2){
-    image1 = "./images/dice" + ran + ".png";
+    image1 = "./dice" + ran + ".png";
     document.querySelectorAll("img")[0].setAttribute("src", image1);
 }
 if(ran===3){
-    image1 = "./images/dice" + ran + ".png";
+    image1 = "./dice" + ran + ".png";
     document.querySelectorAll("img")[0].setAttribute("src", image1);
 }
 if(ran===4){
-    image1 = "./images/dice" + ran + ".png";
+    image1 = "./dice" + ran + ".png";
     document.querySelectorAll("img")[0].setAttribute("src", image1);
 }
 if(ran===5){
-    image1 = "./images/dice" + ran + ".png";
+    image1 = "./dice" + ran + ".png";
     document.querySelectorAll("img")[0].setAttribute("src", image1);
 }
 if(ran===6){
-    image1 = "./images/dice" + ran + ".png";
+    image1 = "./dice" + ran + ".png";
     document.querySelectorAll("img")[0].setAttribute("src", image1);
 }
 
@@ -35,27 +35,27 @@ var ran2= Math.random();
 ran2 = Math.floor(ran2*6)+1;
 
 if(ran2===1){
-    image2 = "./images/dice" + ran2 + ".png";
+    image2 = "./dice" + ran2 + ".png";
     document.querySelectorAll("img")[1].setAttribute("src", image2);
 }
 if(ran2===2){
-    image2 = "./images/dice" + ran2 + ".png";
+    image2 = "./dice" + ran2 + ".png";
     document.querySelectorAll("img")[1].setAttribute("src", image2);
 }
 if(ran2===3){
-    image2 = "./images/dice" + ran2 + ".png";
+    image2 = "./dice" + ran2 + ".png";
     document.querySelectorAll("img")[1].setAttribute("src", image2);
 }
 if(ran2===4){
-    image2 = "./images/dice" + ran2 + ".png";
+    image2 = "./dice" + ran2 + ".png";
     document.querySelectorAll("img")[1].setAttribute("src", image2);
 }
 if(ran2===5){
-    image2 = "./images/dice" + ran2 + ".png";
+    image2 = "./dice" + ran2 + ".png";
     document.querySelectorAll("img")[1].setAttribute("src", image2);
 }
 if(ran2===6){
-    image2 = "./images/dice" + ran2 + ".png";
+    image2 = "./dice" + ran2 + ".png";
     document.querySelectorAll("img")[1].setAttribute("src", image2);
 }
 
